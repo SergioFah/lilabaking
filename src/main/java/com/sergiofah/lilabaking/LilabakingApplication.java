6e6a55232b3f363a35d9fb44582fcce7a55c204b
sergiofah <fahsergio@gmail.com>
2024-06-02 13:10:57 -0300

    - initial commit

---
+++ src/main/java/com/sergiofah/lilabaking/LilabakingApplication.java
@@ -0,0 +1,13 @@
+package com.sergiofah.lilabaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilabakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LilabakingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergiofah/lilabaking/LilabakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergiofah.lilabaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilabakingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
